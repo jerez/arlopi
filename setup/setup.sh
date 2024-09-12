30ce2b4edb9ccc1427519540afadeaa085c13717
Carlos Jerez <jerez@users.noreply.github.com>
2024-09-12 01:02:54 -0500

    Patch configuration

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir -p /home/pi/log
+echo "Update package manager"
+sudo apt-get update
+sudo apt-get install
 echo "Enabling DWC2"
 sudo sh /home/pi/arlopi/setup/enable_dwc2_module.sh
 echo "Creating Storage"
 sudo /bin/bash /home/pi/arlopi/setup/create_storages.sh
-echo "Settup up Samba"
+echo "Setting up Samba"
 sudo sh /home/pi/arlopi/setup/setup_samba.sh
 echo "Setting up Cronjob"
 sh /home/pi/arlopi/setup/setup_cronjob.sh
